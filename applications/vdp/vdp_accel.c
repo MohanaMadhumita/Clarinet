c68bbd6e54403b716f12f905c97f8c031f548ce1
MohanaMadhumita <pmmadhumita9@gmail.com>
2020-11-02 22:07:44 +0530

    vdp update

--- applications/vdp/vdp_accel.c
+++ applications/vdp/vdp_accel.c
@@ -5,6 +5,7 @@
 #include <inttypes.h>
 #include "clarinet.h"
 
+<<<<<<< HEAD
 #define VSZ 16
 
 /* Define the REP macro to indicate number of repetitions to simulate
@@ -38,6 +39,15 @@ static unsigned int p_b [VSZ] = { 0xa6ac1500, 0xaf08ad00, 0xac018400, 0x4c9b9500
 static unsigned int p_a [VSZ] = { 0xa6ac1570, 0xaf08ad60, 0xac0183f0, 0x4c9b9540, 0x52b13500, 0x58803d70, 0xae745430, 0xbc56fb50, 0xbc8b1aa0, 0x48fa0540, 0xa6c2ffa0, 0x31313ff0, 0xb6e6cfe0, 0x5675aa10, 0xb6e64d30, 0xcf62f5a0 };
 static unsigned int p_b [VSZ] = { 0xa6ac1570, 0xaf08ad60, 0xac0183f0, 0x4c9b9540, 0x52b13500, 0x58803d70, 0xae745430, 0xbc56fb50, 0xbc8b1aa0, 0x48fa0540, 0xa6c2ffa0, 0x31313ff0, 0xb6e6cfe0, 0x5675aa10, 0xb6e64d30, 0xcf62f5a0 };
 #endif
+=======
+#define VSZ 24
+
+static float v_a [VSZ]        = {-7.8550171852, -8.9708328247, -2.3183071613, -8.6166706085, 3.0767068863, -7.8940663338, 6.5208415985, 2.5259063244, 3.4050474167, 3.1847724915, 7.3107628822, -5.6170530319, -4.7297372818, 4.5974335670, 6.0904574394, -3.0683014393, 4.1741681099, -3.3324038982, -7.1214756966, 2.7567198277, -3.4179165363, -9.2278394699, 8.9482812881, -0.3960950971};
+static float v_b [VSZ]        = {-7.8550171852, -8.9708328247, -2.3183071613, -8.6166706085, 3.0767068863, -7.8940663338, 6.5208415985, 2.5259063244, 3.4050474167, 3.1847724915, 7.3107628822, -5.6170530319, -4.7297372818, 4.5974335670, 6.0904574394, -3.0683014393, 4.1741681099, -3.3324038982, -7.1214756966, 2.7567198277, -3.4179165363, -9.2278394699, 8.9482812881, -0.3960950971};
+static unsigned int p_a [VSZ] = {0x4400bf40, 0x5600c140, 0x4f7df750, 0x522a4120, 0xb9ffa7e0, 0xafb115e0, 0x5889f220, 0xb20a6420, 0x48be1910, 0xaed8eee0, 0xc75db8a0, 0xaa549e70, 0x5579b790, 0x5946d260, 0x57ff6560, 0xb7c7ce80, 0xa699d5f0, 0x56e2b790, 0x54b0c870, 0xaf905880, 0xa6c3cce0, 0x590f6a50, 0xa8e4ab00, 0xb5d2b4c0};
+static unsigned int p_b [VSZ] = {0x4400bf40, 0x5600c140, 0x4f7df750, 0x522a4120, 0xb9ffa7e0, 0xafb115e0, 0x5889f220, 0xb20a6420, 0x48be1910, 0xaed8eee0, 0xc75db8a0, 0xaa549e70, 0x5579b790, 0x5946d260, 0x57ff6560, 0xb7c7ce80, 0xa699d5f0, 0x56e2b790, 0x54b0c870, 0xaf905880, 0xa6c3cce0, 0x590f6a50, 0xa8e4ab00, 0xb5d2b4c0};
+
+>>>>>>> vdp update
 
 float fn_posit_p_vdp (int r) {
    unsigned int acc = 0;
@@ -70,7 +80,11 @@ float fn_posit_accel_vdp (int r) {
    fn_reset_accel_quire ();
    for (rep=0; rep < r; rep ++)
       for (idx=0; idx<VSZ; idx++) fn_posit_accel_fma (v_a[idx], v_b[idx]);
+<<<<<<< HEAD
    acc = fn_read_accel_quire ();
+=======
+   acc = fn_read_accel_quire();
+>>>>>>> vdp update
    return (acc);
 }
 
@@ -85,6 +99,10 @@ float fn_float_vdp (int r) {
    return (acc);
 }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> vdp update
 float fn_float_optimized_vdp (int r) {
    float acc = 0.0;
    int idx = 0;
@@ -107,6 +125,7 @@ int main (void) {
 
    // Basic VDPs are working
    // Now lets do some cycle analysis
+<<<<<<< HEAD
 #ifdef OPT_FLOAT
    start=read_cycle();
    acc1 = fn_float_optimized_vdp (REP);
@@ -144,5 +163,33 @@ int main (void) {
 #endif
 */
 
+=======
+   start=read_cycle();
+   acc1 = fn_float_optimized_vdp (100);
+   end=read_cycle();
+   printf("Compile Optimized Float VDP: %d cycles have elapsed.\n",(end-start));
+
+   start=read_cycle();
+   acc1 = fn_float_vdp (100);
+   end=read_cycle();
+   printf("Unoptimized Float VDP: %d cycles have elapsed.\n",(end-start));
+
+   start=read_cycle();
+   acc2 = fn_posit_accel_vdp (100);
+   end=read_cycle();
+   printf("Float VDP With Posits: %d cycles have elapsed.\n",(end-start));
+
+/*
+   start=read_cycle();
+   acc2 = fn_posit_vdp (100);
+   end=read_cycle();
+   printf("Float VDP With Posits: %d cycles have elapsed.\n",(end-start));
+
+   start=read_cycle();
+   acc3 = fn_posit_p_vdp (100);
+   end=read_cycle();
+   printf("Native Posit VDP: %d cycles have elapsed.\n",(end-start));
+*/
+>>>>>>> vdp update
    return (0);
 }
